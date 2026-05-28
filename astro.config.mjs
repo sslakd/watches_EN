4466d585cedca32a8039a791615e865813693e60
Son Nguyen <sonnguyen@Sons-Mac-mini.local>
2026-05-28 09:19:18 +0700

    Add sitemap integration + robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://watchstories.store',
   outDir: './dist',
-  build: {
-    format: 'file'
-  }
+  build: { format: 'file' },
+  integrations: [sitemap()]
 });
